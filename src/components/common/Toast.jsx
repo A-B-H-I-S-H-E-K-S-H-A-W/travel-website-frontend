ed2ebbd27074349b643d484a3b1ff162eaf2c4db
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-06-08 15:55:48 +0530

    Changes is toast success message color

--- src/components/common/Toast.jsx
+++ src/components/common/Toast.jsx
@@ -1,31 +1,34 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
-const Toast = ({ result, setResult, color }) => {
+const Toast = ({ result, setResult }) => {
   useEffect(() => {
     if (result) {
       const timer = setTimeout(() => {
         setResult(null);
-      }, 4000); // 4 seconds
+      }, 4000);
       return () => clearTimeout(timer);
     }
   }, [result, setResult]);
 
+  if (!result) return null;
+
+  const bgColor = result.success ? "bg-green-500" : "bg-red-500";
+  const textColor = "text-white";
+
   return (
     <AnimatePresence>
-      {result && (
-        <motion.div
-          id="toast-bottom-right"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 50 }}
-          transition={{ duration: 0.4 }}
-          className={`fixed flex items-center w-full max-w-xs p-4 space-x-4 divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow-sm right-5 bottom-5 dark:text-gray-400 dark:divide-gray-700 dark:bg-gray-800 ${color}`}
-          role="alert"
-        >
-          <div className="text-sm font-semibold">{result.message}</div>
-        </motion.div>
-      )}
+      <motion.div
+        id="toast-bottom-right"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 50 }}
+        transition={{ duration: 0.4 }}
+        className={`fixed flex items-center w-full max-w-xs p-4 rounded-lg shadow-md right-5 bottom-5 z-50 ${bgColor} ${textColor}`}
+        role="alert"
+      >
+        <div className="text-sm font-semibold">{result.message}</div>
+      </motion.div>
     </AnimatePresence>
   );
 };
