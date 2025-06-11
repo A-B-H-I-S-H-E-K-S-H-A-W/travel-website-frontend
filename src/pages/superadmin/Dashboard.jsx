127300d8a9928f6f2d3b9b32a57504cc9dba24e8
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-06-11 17:07:54 +0530

    Minor bug and desgin fixes

--- src/pages/superadmin/Dashboard.jsx
+++ src/pages/superadmin/Dashboard.jsx
@@ -1,9 +1,6 @@
 import SuperAdminLayout from "./layout/Layout";
 
 const AdminDashboard = () => {
-  const token = localStorage.getItem("superAdminToken");
-  console.log(token);
-
   return (
     <SuperAdminLayout>
       <div>Dashboard</div>
